0a0738de5b5aa1370dd6892dc35de02b7210d595
Adonys <avaldesa007@gmail.com>
2020-04-05 18:38:53 -0400

    Actualizar puntos

---
+++ application/controllers/Home.php
@@ -0,0 +1,35 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Table_Model');
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->library('form_validation');
+        $this->load->library('session');
+	}
+	
+
+	public function index()
+	{
+		$this->load->view('game/index');
+		
+	}
+	public function updatePoints(){
+
+		$name  = $this->input->post('name');
+		$record = $this->input->post('record');
+		$data = $arrayName = array('username' => $name,'record'=>$record );
+		$this->Table_Model->insertRecords($data);
+	}
+
+}
+
+/* End of file Home.php */
+
---
+++ application/models/Table_Model.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Table_Model extends CI_Model {
+	
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+	}
+	
+
+	function getRecords(){
+		$query = $this->db->get('user_records');
+        return $query->row();
+	}
+    public function insertRecords($data)
+    {
+        if (isset($data)) {
+            $this->db->insert('user_records', $data);
+            return true;
+        } else {
+            return false;
+        }
+    }	
+
+}
+
+/* End of file ModelName.php**/
+
